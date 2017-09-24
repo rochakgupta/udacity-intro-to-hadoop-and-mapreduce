737ade964e4c92a8de46f865a46d8d5a68dd083b
Rochak Gupta <rochak84@gmail.com>
2017-09-25 00:14:13 +0530

    Sales exercise-1 task-1

---
+++ sales/exercise-1/task-1/code/reducer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import sys
+
+salesTotal = 0
+oldKey = None
+
+for line in sys.stdin:
+    data_mapped = line.strip().split("\t")
+    if len(data_mapped) != 2:
+        continue
+
+    thisKey, thisSale = data_mapped
+
+    if oldKey and oldKey != thisKey:
+        print oldKey, "\t", salesTotal
+        oldKey = thisKey
+        salesTotal = 0
+
+    oldKey = thisKey
+    salesTotal += float(thisSale)
+
+if oldKey is not None:
+    print oldKey, "\t", salesTotal
