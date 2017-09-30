8962d412c1b94f8089c4ae1fbf85810840a6056f
Rochak Gupta <rochak84@gmail.com>
2017-09-30 15:18:36 +0530

    Web server log exercise-1 task-1

---
+++ web-server-log/exercise-1/task-1/code/mapper.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import sys
+
+for line in sys.stdin:
+    data = line.strip().split(" ")
+    if len(data) == 10:
+        ip_address, client_id, client_username, time, time_zome, request_method, file_name, protocol, status_code, size\
+            = data
+        print file_name
---
+++ web-server-log/exercise-1/task-1/code/reducer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import sys
+
+hits = 0
+oldKey = None
+
+for line in sys.stdin:
+    thisKey = line.strip()
+
+    if oldKey and oldKey != thisKey:
+        print oldKey, " ", hits
+        oldKey = thisKey
+        hits = 0
+
+    oldKey = thisKey
+    hits += 1
+
+if oldKey is not None:
+    print oldKey, " ", hits
